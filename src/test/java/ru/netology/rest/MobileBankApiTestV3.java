eddd6816725c2a005ed433a2f24e532548f472eb
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2023-01-14 13:58:06 +0300

    Change RUB to RUR test fall

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -19,16 +19,22 @@ class MobileBankApiTestV3 {
                 .body("", hasSize(3))
                 .body("[0].currency", equalTo("RUB"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
+                .body("[0].name", equalTo("Текущий счёт"))
         ;
     }
 
     @Test
-    void should2Accounts() {
+    void shouldReturnDemoAccounts_USD() {
         given()
                 .baseUri("http://localhost:9999/api/v1")
                 .when()
                 .get("/demo/accounts")
                 .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUR"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
                 .body("[0].name", equalTo("Текущий счёт"))
         ;
     }
